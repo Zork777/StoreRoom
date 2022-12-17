3b9e01f358566435d5c1daa183aafc15f46740bf
zork777 <zork777@yandex.ru>
2022-12-17 20:06:03 +0300

    v.1.0.1 add en and ru "name" in test data

--- StoreRoom/View/GenerateTestData.swift
+++ StoreRoom/View/GenerateTestData.swift
@@ -14,17 +14,21 @@ import UIKit
  */
 
 class TestData: BaseCoreData {
-    let testRooms = [Object(name: "Кладовка1", image: UIImage(named: "room1")!),
+    var testRooms = [Object]()
+    var testBoxs = [Object]()
+    var testThings = [Object]()
+    
+    let testRoomsRu = [Object(name: "Кладовка1", image: UIImage(named: "room1")!),
                  Object(name: "Кладовка2", image: UIImage(named: "room2")!),
                  Object(name: "Гараж", image: UIImage(named: "room3")!),
                  Object(name: "Шкаф", image: UIImage(named: "room4")!)]
     
-    let testBoxs = [Object(name: "Коробка", image: #imageLiteral(resourceName: "box1.jpg")),
+    let testBoxsRu = [Object(name: "Коробка", image: #imageLiteral(resourceName: "box1.jpg")),
                 Object(name: "Контейнер", image: #imageLiteral(resourceName: "box2.jpg")),
                 Object(name: "Коробка2", image: #imageLiteral(resourceName: "box3.jpg")),
                 Object(name: "Контейнер с игрушками", image: #imageLiteral(resourceName: "box4.jpg"))]
     
-    let testThings = [Object(name: "босоножки", image: #imageLiteral(resourceName: "shoes5.jpg")),
+    let testThingsRu = [Object(name: "босоножки", image: #imageLiteral(resourceName: "shoes5.jpg")),
                   Object(name: "туфли", image: #imageLiteral(resourceName: "shoes6.jpg")),
                   Object(name: "футболка", image: #imageLiteral(resourceName: "футболка.jpeg")),
                   Object(name: "Лопата", image: #imageLiteral(resourceName: "лопата.jpg")),
@@ -33,12 +37,47 @@ class TestData: BaseCoreData {
                   Object(name: "Свитер", image: #imageLiteral(resourceName: "свитер.jpg")),
                   Object(name: "сумка", image: #imageLiteral(resourceName: "сумка.jpg")),
                   Object(name: "Джинсы", image: #imageLiteral(resourceName: "джинсы.jpeg")),
-                  Object(name: "toy1", image: #imageLiteral(resourceName: "toy1.jpg")),
-                  Object(name: "toy2", image: #imageLiteral(resourceName: "toy2.jpg"))]
+                  Object(name: "Стич", image: #imageLiteral(resourceName: "toy1.jpg")),
+                  Object(name: "Игрушка", image: #imageLiteral(resourceName: "toy2.jpg"))]
+    
+    let testRoomsEn = [Object(name: "Storeroom1", image: UIImage(named: "room1")!),
+                 Object(name: "Storeroom2", image: UIImage(named: "room2")!),
+                 Object(name: "Garage", image: UIImage(named: "room3")!),
+                 Object(name: "Wardrobe", image: UIImage(named: "room4")!)]
+    
+    let testBoxsEn = [Object(name: "Box", image: #imageLiteral(resourceName: "box1.jpg")),
+                Object(name: "Container", image: #imageLiteral(resourceName: "box2.jpg")),
+                Object(name: "Box2", image: #imageLiteral(resourceName: "box3.jpg")),
+                Object(name: "Container with toys", image: #imageLiteral(resourceName: "box4.jpg"))]
+    
+    let testThingsEn = [Object(name: "sandals", image: #imageLiteral(resourceName: "shoes5.jpg")),
+                  Object(name: "shoes", image: #imageLiteral(resourceName: "shoes6.jpg")),
+                  Object(name: "T-shirt", image: #imageLiteral(resourceName: "футболка.jpeg")),
+                  Object(name: "Shovel", image: #imageLiteral(resourceName: "лопата.jpg")),
+                  Object(name: "Helmet", image: #imageLiteral(resourceName: "каска.jpg")),
+                  Object(name: "Barbecue", image: #imageLiteral(resourceName: "мангал.jpg")),
+                  Object(name: "Sweater", image: #imageLiteral(resourceName: "свитер.jpg")),
+                  Object(name: "bag", image: #imageLiteral(resourceName: "сумка.jpg")),
+                  Object(name: "Jeans", image: #imageLiteral(resourceName: "джинсы.jpeg")),
+                  Object(name: "Stitch", image: #imageLiteral(resourceName: "toy1.jpg")),
+                  Object(name: "toy", image: #imageLiteral(resourceName: "toy2"))]
+    
+    
     let base = BaseCoreData()
     
     override init(){
         super.init()
+
+        if let languages = Locale.preferredLanguages.first, languages.contains("ru") {
+            testRooms = testRoomsRu
+            testBoxs = testBoxsRu
+            testThings = testThingsRu
+        }
+        else {
+            testRooms = testRoomsEn
+            testBoxs = testBoxsEn
+            testThings = testThingsEn
+        }
     }
     
 ///чистим всю базу
